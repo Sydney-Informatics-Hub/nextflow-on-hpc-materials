f65c23cd2ed1ddf0c871c326a937e7917232205f
mgeaghan <michael.p.geaghan@gmail.com>
2025-11-10 19:04:33 +1100

    update part1 scripts to run mapping instead of markduplicates

--- part1/scripts/run.slurm.sh
+++ part1/scripts/run.slurm.sh
@@ -4,13 +4,15 @@ module load nextflow/24.10.0
 module load singularity/4.1.0-slurm
 
 nextflow run sarek/main.nf \
-    --input ../data/bams/samplesheet.csv \
-    --step markduplicates \
-    --skip_tools baserecalibrator,mosdepth,samtools \
-    --outdir results \
-    --no_intervals true \
+    --input ../data/fqs/samplesheet.single.csv \
     --fasta ../data/ref/Hg38.subsetchr20-22.fasta \
     --fasta_fai ../data/ref/Hg38.subsetchr20-22.fasta.fai \
+    --dict ../data/ref/Hg38.subsetchr20-22.dict \
+    --bwa ../data/ref \
+    --step mapping \
+    --skip_tools markduplicates,baserecalibrator,mosdepth,samtools \
+    --outdir results \
+    --no_intervals true \
     --igenomes_ignore true \
     -c config/setonix.config,config/custom.config \
     -resume
