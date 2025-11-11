540e13804cd60f48ca0cd4475830cb7e5523cf16
mgeaghan <michael.p.geaghan@gmail.com>
2025-11-11 14:04:51 +1100

    update for new singularity paths

--- part1/scripts/pull_sarek.pbs.sh
+++ part1/scripts/pull_sarek.pbs.sh
@@ -4,6 +4,3 @@ set -euo pipefail
 
 # Clone sarek repository at version 3.5.0
 git clone -b 3.5.0 git@github.com:nf-core/sarek.git
-
-# Copy relevant singularity images
-cp -r /scratch/vp91/singularity sarek/
--- part1/scripts/pull_sarek.slurm.sh
+++ part1/scripts/pull_sarek.slurm.sh
@@ -4,6 +4,3 @@ set -euo pipefail
 
 # Clone sarek repository at version 3.5.0
 git clone -b 3.5.0 git@github.com:nf-core/sarek.git
-
-# Copy relevant singularity images
-cp -r /scratch/courses01/singularity sarek/
